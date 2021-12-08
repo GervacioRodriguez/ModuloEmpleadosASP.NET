6fda88eec513ce4e8c240a6a8549f66ef31a2b32
pablo antonio gervacio rodriguez <gervaciopabloantonio@outlook.com>
2021-12-08 07:25:32 -0600

    se agrego las instancias requeridas

--- modulosASP/AdministracionVacantes.aspx.cs
+++ modulosASP/AdministracionVacantes.aspx.cs
@@ -4,12 +4,27 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace modulosASP
 {
     public partial class AdministracionVacantes : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                iniciaarllenado();
+
+            }
+        }
+        private void iniciarllenado dropdwon()
+        {
+            
+        }
+        
+        public DataSet consultar(string stringsql)
         {
 
         }
