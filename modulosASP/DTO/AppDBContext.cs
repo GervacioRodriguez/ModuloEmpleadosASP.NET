b7e79e65ad2e170017ace27bc7f5a92664d0c7ab
pablo antonio gervacio rodriguez <gervaciopabloantonio@gmail.com>
2024-03-16 11:46:05 -0600

    SE desea generar las clases de dto apartir de linq to sql tratando de automatizar la creacion de clases

---
+++ modulosASP/DTO/AppDBContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace modulosASP.DTO
+{
+    public class AppDBContext:DbContext
+    {
+    }
+}
