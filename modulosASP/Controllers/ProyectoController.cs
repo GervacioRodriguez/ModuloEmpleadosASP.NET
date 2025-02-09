23faf0bcb80749168c7fb649721c8502e68a7d46
pablo antonio gervacio rodriguez <gervaciopabloantonio@gmail.com>
2025-02-09 01:56:25 -0600

    se estan haciendo los proyectos para subir al sistem

---
+++ modulosASP/Controllers/ProyectoController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using modulosASP.Models;
+using modulosASP.DTO;
+
+namespace modulosASP.Controllers
+{
+    public class ProyectoController : Controller
+    {
+        // GET: Proyecto
+        public ActionResult Index()
+        {
+            //generamos la lista de datos a imprimir
+            List<ProyectoDTO> listaproyectos = null;
+            //llamamos la conexion de la base de datos
+            using (var db = new SEICEntities())
+            {
+                listaproyectos = (from pr in db.C_Proyecto
+                                  select new ProyectoDTO
+                                  {
+                                      Id = pr.Id,
+                                      Proyecto = pr.Proyecto,
+                                      Tipo = pr.Tipo,
+                                      Clave = pr.Clave,
+                                      Vigente = pr.Vigente
+                                  }).ToList();
+            }
+
+                return View(listaproyectos);
+        }
+    }
+}
---
+++ modulosASP/DTO/ProyectoDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace modulosASP.DTO
+{
+    public class ProyectoDTO
+    {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name ="Nombre Del Proyecto")]
+        public string Proyecto { get; set; }
+        [Display(Name = "Tipo de Proyecto")]
+        public string Tipo { get; set; }
+        [Display(Name ="Clave Del Proyecto")]
+        public string Clave { get; set; }
+        [Display(Name ="Vigencia Del Proyecto")]
+        public Nullable<int> Vigente { get; set; }
+    }
+}
